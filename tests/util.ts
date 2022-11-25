d54684eb7a737474472bc4ba7fc3b7583846e664
Alex <117545308+alexanderjkslfj@users.noreply.github.com>
2022-11-25 23:49:31 +0100

    fix vocal check

--- tests/util.ts
+++ tests/util.ts
@@ -4,14 +4,17 @@ export function testAll(tests: (() => Promise<boolean>)[], vocal: boolean = true
         for (const test of tests) {
             try {
                 const success = await test()
-                if(success) {
-                    console.log(`Test ${test.name} finished successfully.`)
+                if (success) {
+                    if (vocal)
+                        console.log(`Test ${test.name} finished successfully.`)
                 } else {
-                    console.log(`Test ${test.name} didn't finish successfully. It returned a wrong result.`)
+                    if (vocal)
+                        console.log(`Test ${test.name} didn't finish successfully. It returned a wrong result.`)
                     works = false
                 }
             } catch {
-                console.log(`Test ${test.name} didn't finish successfully. It threw an error.`)
+                if (vocal)
+                    console.log(`Test ${test.name} didn't finish successfully. It threw an error.`)
                 works = false
             }
         }
