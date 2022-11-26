2691b1d58cc2f66236637e9fdb2398233c5176df
Alex <117545308+alexanderjkslfj@users.noreply.github.com>
2022-11-26 13:17:28 +0100

    add tests

--- tests/general.ts
+++ tests/general.ts
@@ -4,7 +4,9 @@ import * as general from "../src/general.js"
 
 const testGeneral = testAll([
     serializeDeserializePrimitives,
-    serializeDeserializeObject
+    serializeDeserializeObject,
+    stringifyParseFunction,
+    inlineWorker
 ])
 
 export default testGeneral
@@ -57,4 +59,51 @@ async function serializeDeserializeObject(): Promise<[boolean, [object, any]]> {
     const aclone = general.deserialize(general.serialize(a))
 
     return [((a.b.c.num === aclone.b.c.num) && (aclone.b.c.d.c.d.c === aclone.b.c)), [a, aclone]]
+}
+
+async function stringifyParseFunction(): Promise<[boolean, [CallableFunction, CallableFunction][]]> {
+    function a(x: number): number {
+        return x + 1
+    }
+
+    const b = function (x: number): number {
+        return x + 1
+    }
+
+    const c = (x: number): number => {
+        return x + 1
+    }
+
+    const newa = general.str2obj(general.obj2str(a))
+    const newb = general.str2obj(general.obj2str(b))
+    const newc = general.str2obj(general.obj2str(c))
+
+    const resulta = newa(1)
+    const resultb = newb(2)
+    const resultc = newc(3)
+
+    return [
+        (resulta === 2 && resultb === 3 && resultc === 4),
+        [[a, newa], [b, newb], [c, newc]]
+    ]
+}
+
+function inlineWorker(): Promise<[boolean, [number, any]]> {
+    return new Promise(res => {
+        const worker = general.inlineWorker(function () {
+            // @ts-ignore
+            const window = this
+
+            window.addEventListener("message", (e: any) => {
+                window.postMessage(e.data)
+            })
+        })
+
+        worker.addEventListener("message", e => {
+            res([e.data === 5, [5, e.data]])
+            worker.terminate()
+        })
+
+        worker.postMessage(5)
+    })
 }
