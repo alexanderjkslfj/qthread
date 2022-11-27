1550ca356d4df5123202d742c8b186999a0748ee
Alex <117545308+alexanderjkslfj@users.noreply.github.com>
2022-11-27 19:14:19 +0100

    add test

--- tests/cluster.ts
+++ tests/cluster.ts
@@ -4,6 +4,7 @@ import Cluster from "../src/cluster.js";
 const testCluster = testAll([
     threadCount,
     callMethod,
+    callMethods,
     addMethodAfterAddingThread
 ])
 
@@ -32,6 +33,29 @@ async function callMethod(): Promise<[boolean, number]> {
     const result = await cluster.callMethod<number>("add", 1, 2)
 
     return [result === 3, result]
+}
+
+async function callMethods(): Promise<[boolean, number[]]> {
+    const cluster = new Cluster()
+
+    await cluster.addThread()
+
+    await cluster.addMethod((a: number, b: number): number => {
+        return a + b
+    }, "add")
+
+    const results = await Promise.all([
+        await cluster.callMethod<number>("add", 1, 0),
+        await cluster.callMethod<number>("add", 1, 1),
+        await cluster.callMethod<number>("add", 1, 2)
+    ])
+
+    cluster.terminate()
+
+    return [
+        (results[0] === 1 && results[1] === 2 && results[2] === 3),
+        results
+    ]
 }
 
 async function addMethodAfterAddingThread(): Promise<[boolean, number]> {
