132309e2d74f56c0e7fd829de3f33668fd499d04
Alex <117545308+alexanderjkslfj@users.noreply.github.com>
2022-11-28 01:56:46 +0100

    add exports to index

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,26 @@ import Thread from "./thread.js";
 import Cluster from "./cluster.js";
 
 const inlineWorker = general.inlineWorker
+const serialize = general.serialize
+const serializeAll = general.serializeAll
+const deserialize = general.deserialize
+const deserializeAll = general.deserializeAll
+const obj2str = general.obj2str
+const str2obj = general.str2obj
+
+type serializable = general.serializable
+type serialized = general.serialized
 
 export {
     Thread,
     Cluster,
-    inlineWorker
+    inlineWorker,
+    serialize,
+    serializeAll,
+    deserialize,
+    deserializeAll,
+    obj2str,
+    str2obj,
+    serializable,
+    serialized
 }
